51d8eda503700cbd2b1a58904ac85f3e2fc5fef9
Celebrian <per_kb@hotmail.com>
2018-12-01 18:26:20 +0100

    added output for tasks, added day1 part2

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
-import "github.com/Celebrian/Advent-of-Code-2018/Day1"
+import (
+	"fmt"
+
+	"github.com/Celebrian/Advent-of-Code-2018/Day1"
+)
 
 func main() {
 	//Day 1
+	fmt.Println("\nDay1 Part1")
 	Day1.Part1()
+
+	fmt.Println("\nDay1 Part2")
+	Day1.Part2()
 
 }
