aa5c2904a0f8f0c39ae729e8825630893a73b012
Celebrian <per_kb@hotmail.com>
2018-12-03 17:56:24 +0100

    Added verbose flag for testing

--- Day3/day3A_test.go
+++ Day3/day3A_test.go
@@ -1,6 +1,17 @@
 package day3
 
-import "testing"
+import (
+	"flag"
+	"testing"
+)
+
+var verbose *bool
+
+//Function to parse verbose flag
+func init() {
+	verbose = flag.Bool("v", false, "Should the program be verbose or not(default not).")
+	flag.Parse()
+}
 
 //Test day3A function
 func TestA(t *testing.T) {
@@ -11,7 +22,7 @@ func TestA(t *testing.T) {
 
 	//For each case, check to see if A returns the expected result.
 	for input, expected := range testCases {
-		actual := A(input, false)
+		actual := A(input, *verbose, 8)
 
 		if actual != expected {
 			t.Error("Failure: Expected ", expected, " got ", actual, "\n")
